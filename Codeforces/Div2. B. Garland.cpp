a163eaa7682635eda47bccfdf24f1a9945573b2d
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2022-02-05 02:19:28 +0200

    Update and rename Div2. B- Garland.txt to Div2. B. Garland.cpp

--- Codeforces/Div2. B. Garland.cpp
+++ Codeforces/Div2. B. Garland.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 int main()
 {
-    { ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); } // to fast input and output.
-    string s, v; cin >> s >> v;
-    vector<int> count(26, 0); vector<bool>isext(26);
+        { ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); } // to fast input and output.
+        string s, v; cin >> s >> v;
+        vector<int> count(26, 0); vector<bool>isext(26);
 	int area = 0;
 	for (int i = 0; i < s.size(); i++) count[s[i] - 97]++, isext[s[i] - 97] = 1;
 	for (int i = 0; i < v.size(); i++)

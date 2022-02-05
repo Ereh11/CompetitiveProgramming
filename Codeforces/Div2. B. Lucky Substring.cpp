ecc5ef32f6fbc02f334aad34a609c93660e94be3
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2022-02-05 03:00:12 +0200

    Update and rename Div2. B. Lucky Substring.txt to Div2. B. Lucky Substring.cpp

--- Codeforces/Div2. B. Lucky Substring.cpp
+++ Codeforces/Div2. B. Lucky Substring.cpp
@@ -9,7 +9,7 @@ int main()
       string s; cin >> s;
 	int co_7 = 0, co_4 = 0;
 	for (int i = 0; i < s.size(); i++)
-		  if (s[i] == '4') co_4++;
+		if (s[i] == '4') co_4++;
 	        else if(s[i]=='7') co_7++;
 	if (!co_4 && !co_7) cout << -1;
 	else if(co_4 >= co_7) cout << 4;

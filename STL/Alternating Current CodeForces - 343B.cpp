6e49ef140c75458b0f2a80018bb2c94a430d90c6
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2022-06-30 18:36:49 +0200

    Add files via upload

--- STL/Alternating Current CodeForces - 343B.cpp
+++ STL/Alternating Current CodeForces - 343B.cpp
@@ -4,30 +4,16 @@ using namespace std;
 int main()
 {
     FastIO;
-    int t, n; cin >> t;
-	while (t-- && cin >> n)
+    string s; cin >> s;
+	stack<char> st;
+	for (auto& it : s)
 	{
-		vector<int> v(n), sfx[10];
-		int indx1 = -1, indx2 = -1;
-		for (auto& it : v) cin >> it;
-		sfx[v[n - 1]].push_back(n - 1);
-		for (int i = n - 2; i >= 0; i--)
+		if (st.empty()) st.push(it);
+		else
 		{
-			for (int j = v[i] + 1; j <= 9; j++)
-				if (!sfx[j].empty()) {
-					indx1 = i, indx2 = j;
-					break;
-				}
-			if (~indx1) break;
-			sfx[v[i]].push_back(i);
+			if (st.top() == '+' && it == '+' || st.top() == '-' && it == '-') st.pop();
+			else st.push(it);
 		}
-		if (~indx1)
-		{
-			swap(v[indx1], v[sfx[indx2].front()]);
-			sort(v.begin() + indx1 + 1, v.end());
-			for (auto& it : v) cout << it;
-		}
-		else cout << -1;
-		cout << ((t) ? "\n" : "");
 	}
+	cout << ((st.empty()) ? "Yes" : "No");
 }

284ad3fa91f0a1752668ed3a77a2b80968323cd4
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2022-07-10 09:24:40 +0200

    BFS

--- Graph problems/Cthulhu CodeForces - 103B.cpp
+++ Graph problems/Cthulhu CodeForces - 103B.cpp
@@ -4,21 +4,25 @@ using namespace std;
 const int N = 101;
 int cntnode = 0, cntCycle = 0;
 vector<int> edge[N], vis;
-void DFS(int node)
+void BFS()
 {
-	vis[node] = 1, cntnode++;
-	for (auto& child : edge[node]) 
-	{
-		if (!vis[child]) DFS(child);
-		else if(vis[child] == 2) cntCycle++;
-	}
-	vis[node] = 2;
+	 queue<int> qu; qu.push(1);
+	 while (!qu.empty())
+	 {
+		 int ft = qu.front();
+		 qu.pop(), vis[ft] = 2, cntnode++;
+		 for (auto& child : edge[ft]) 
+		 {
+			 if (!vis[child]) qu.push(child), vis[child] = 1;
+			 else if(vis[child] == 1) cntCycle++;
+		 }
+	 }
 }
 int main()
 {
 	FastIO;
 	int n, m; cin >> n >> m; vis.resize(n + 1);
 	for (int i = 0, u, v; i<m && cin >> u >> v; i++) edge[v].push_back(u), edge[u].push_back(v);
-	DFS(1);
+    BFS();
 	cout << ((cntnode == n && cntCycle == 1) ? "FHTAGN!" : "NO");
 }

6515ea78b3ca08c9f27efe5a4cda478525118b42
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-02-11 19:08:48 +0200

    BFS

---
+++ Graph problems/365. Water and Jug Problem.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    
+    bool BFS(int &j1, int &j2, int &target)
+    {
+        vector<int> vis(1e6 + 1);
+        queue<int> qu; 
+        qu.push(0); vis[0] = 1;
+        int operation[4] = {j1, -j1, j2, -j2};
+        
+        while(!qu.empty())
+        {
+            int ft = qu.front(); qu.pop();
+            if(ft == target) return true;
+            
+            for(int i = 0 ; i < 4 ; i++) 
+            {
+                int nw = ft + operation[i];
+                if(nw >= 0 && nw <= (j1 + j2) && !vis[nw])
+                    vis[nw] = 1, qu.push(nw);
+            }
+        }
+        
+        return false;
+    }
+    
+    
+    bool canMeasureWater(int cap1, int cap2, int targetCapacity) 
+    {
+        return BFS(cap1, cap2, targetCapacity);
+    }
+};
+
+
+
+/*
+
+*/
